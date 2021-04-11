25dbef6e83bb59c187de93a0927bba246393936a
Jan Paula San Juan <janpaulasanjuan@gmail.com>
2021-04-11 18:30:37 +0800

    commit get and post

---
+++ Jan-Paula-San-Juan--Group-1--GET-and-POST/result.php
@@ -0,0 +1,10 @@
+<?=
+	"<div>
+		<h1>Submitted Information</h1>
+		<p>Name: {$_POST['user_name']}</p>
+		<p>Dojo Location: {$_POST['location']}</p>
+		<p>Favorite Language: {$_POST['language']}</p>
+		<p>Comment: {$_POST['comment']}</p>
+		<a href='index.html'>Go Back</a>
+	</div>";
+?>
